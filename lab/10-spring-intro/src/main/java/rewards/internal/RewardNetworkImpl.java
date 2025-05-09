27ef34fed08565f44388f99dfb22fee54a396036
moshiur-rahman-mirage <moshiur.mirage@gmail.com>
2025-05-09 09:56:07 +0600

    working on spring academy lab

--- lab/10-spring-intro/src/main/java/rewards/internal/RewardNetworkImpl.java
+++ lab/10-spring-intro/src/main/java/rewards/internal/RewardNetworkImpl.java
@@ -1,9 +1,13 @@
 package rewards.internal;
 
+import common.money.MonetaryAmount;
+import rewards.AccountContribution;
 import rewards.Dining;
 import rewards.RewardConfirmation;
 import rewards.RewardNetwork;
+import rewards.internal.account.Account;
 import rewards.internal.account.AccountRepository;
+import rewards.internal.restaurant.Restaurant;
 import rewards.internal.restaurant.RestaurantRepository;
 import rewards.internal.reward.RewardRepository;
 
@@ -49,10 +53,17 @@ public class RewardNetworkImpl implements RewardNetwork {
 		this.rewardRepository = rewardRepository;
 	}
 
+	@Override
 	public RewardConfirmation rewardAccountFor(Dining dining) {
 		// TODO-07: Write code here for rewarding an account according to
 		//          the sequence diagram in the lab document
 		// TODO-08: Return the corresponding reward confirmation
-		return null;
+		Account account= accountRepository.findByCreditCard(dining.getCreditCardNumber());
+		Restaurant restaurant= restaurantRepository.findByMerchantNumber(dining.getMerchantNumber());
+		MonetaryAmount monetaryAmount = restaurant.calculateBenefitFor(account,dining);
+	AccountContribution contribution= account.makeContribution(monetaryAmount);
+	accountRepository.updateBeneficiaries(account);
+	return rewardRepository.confirmReward(contribution,dining);
+
 	}
 }
--- lab/12-javaconfig-dependency-injection/src/test/java/config/RewardNetworkTests.java
+++ lab/12-javaconfig-dependency-injection/src/test/java/config/RewardNetworkTests.java
@@ -1,4 +1,22 @@
 package config;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import rewards.RewardNetwork;
+import rewards.TestInfrastructureConfig;
+
+import static org.assertj.core.api.Fail.fail;
+
 public class RewardNetworkTests {
+    private RewardNetwork network;
+    @BeforeEach
+    void setUp() throws Exception{
+        ApplicationContext context = SpringApplication.run(TestInfrastructureConfig.class);
+        context.getBean(RewardNetwork.class);
+    }
+
+    void test(){
+        fail("not yest");
+    }
 }
